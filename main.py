673b37242bb02416e4f48d61ced1061b622e2ba6
Ludivine Ducamp <34969114+Wallnya@users.noreply.github.com>
2021-11-22 17:43:08 +0100

    Update questions et design

--- main.py
+++ main.py
@@ -12,8 +12,9 @@ ANNONCE = os.getenv('DISCORD_ANNONCE')
 #Variables
 bot = commands.Bot(command_prefix = "!",description ="Bot lumino test")
 
-list_OriginalQuestions = ["Pour ou contre l'école à la maison ?","Pour ou contre les uniformes scolaires ?","Sommes-nous dépendant des ordinateurs ?","Faut-il obligatoirement être un esprit torturé pour être un grand artiste ?","Pourquoi les gens aiment-ils regarder des vidéos de chats amusantes ?","Quel serait ton animal totem ?","Si tu pouvais poser une question à Dieu, laquelle serait-elle ?","Montre nous ta dernière création artistique de la semaine !","C'est la semaine du complimet alors n'hésitez pas à complimenter votre prochain sur le serveur!","Quel plat pourrais-tu manger sans t'en lasser ?","Plutôt soleil ou plutôt pluie ? ","La question de l'écologie est importante pour vous ?"]
+list_OriginalQuestions = ["```Pour ou contre les uniformes scolaires ?```","```Sommes-nous dépendants des ordinateurs?```","```Faut-il obligatoirement être un esprit torturé pour être un grand artiste ?```","```Pourquoi les gens aiment-ils regarder des vidéos de chats amusantes ?```","```Quel serait ton animal totem ?```","```Si tu pouvais poser une question à Dieu, laquelle serait-elle ?```","```Montre nous ta dernière création artistique de la semaine !```","```C'est la semaine du complimet alors n'hésitez pas à complimenter votre prochain sur le serveur!```","```Quel plat pourrais-tu manger sans t'en lasser ?```","```Plutôt soleil ou plutôt pluie ? ```","```La question de l'écologie est importante pour vous ?```"]
 list_QuestionsAsked = list_OriginalQuestions
+regles = "```Les règles sont simples : toujours répondre à la question qui est posée ou le défi. On ne joue pas les demi-mesures et on répond par oui ou par non quand le besoin y est! \nJe compte sur vous pour ramener à la vie ce petit channel qui n'attend que vos réponses!```"
 
 #Event de lancement
 @bot.event
@@ -22,7 +23,7 @@ async def on_ready():
 @bot.command()
 async def challengeRestants(ctx):
     valeur = len(list_QuestionsAsked)
-    textDisplay = "Il y a " + str(valeur) +" questions restantes dans la base de données."
+    textDisplay = "<@&" + str(ANNONCE) + "> Il reste " + str(valeur) +" questions restantes dans la base de données."
     await ctx.send(textDisplay+" <:teehee:671089198316650524>")
 @bot.command()
 async def challengeAleatoire(ctx):
@@ -30,16 +31,16 @@ async def challengeAleatoire(ctx):
 	valeur = list_QuestionsAsked[n]
 	list_QuestionsAsked.pop(n)
 	print(list_QuestionsAsked)
-	await ctx.send("```<@&" + str(ANNONCE) + ">\n"+valeur+"```")
+	await ctx.send("<@&" + str(ANNONCE) + ">\n"+valeur)
 @bot.command()
 async def challengeNumero(ctx, *, indice):
 	valeur = list_QuestionsAsked[int(indice)]
 	list_QuestionsAsked.pop(int(indice))
 	print(list_QuestionsAsked)
-	await ctx.send("```<@&" + str(ANNONCE) + ">\n"+valeur+"```")
+	await ctx.send("<@&" + str(ANNONCE) + ">\n"+valeur)
 
 @bot.command()
 async def regles(ctx):
-	await ctx.send("```Les règles sont simples : toujours répondre à la question qui est posée ou le défi. On ne joue pas les demi-mesures et on répond par oui ou par non quand le besoin y est! \nJe compte sur vous pour ramener à la vie ce petit channel qui n'attend que vos réponses!```")
+	await ctx.send(regles)
 
 bot.run(TOKEN)
